556c728197cb00952abd8171f7ce8e767032c3cd
Benjamin Schenker <binsch@gmail.com>
2017-01-02 22:20:50 -0600

    elm-format file

--- src/bouncing-box.elm
+++ src/bouncing-box.elm
@@ -1,3 +1,5 @@
+module Main exposing (..)
+
 import Html exposing (Html, button, div, text)
 import Html.App as Html
 import Html.Attributes exposing (..)
@@ -5,17 +7,18 @@ import Html.Events exposing (onClick)
 import Window exposing (..)
 import Style exposing (..)
 import Time exposing (Time, millisecond)
+
+
 -- import Debug  exposing(log)
 
 
-
 main =
-  Html.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
@@ -23,16 +26,17 @@ main =
 
 
 type alias Model =
-  { position : Int
-  , points: Int
-  , speed: Int
-  , direction: Int
-  , viewWidth: Int
-  }
+    { position : Int
+    , points : Int
+    , speed : Int
+    , direction : Int
+    , viewWidth : Int
+    }
 
-init : (Model, Cmd Msg)
+
+init : ( Model, Cmd Msg )
 init =
-  (Model 30 0 1 1 500, Cmd.none)
+    ( Model 30 0 1 1 500, Cmd.none )
 
 
 
@@ -40,27 +44,38 @@ init =
 
 
 type Msg
-  = Tick Time
-  | Point
+    = Tick Time
+    | Point
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Tick newTime ->
-      if model.position > model.viewWidth then
-        ({ model | direction = -1
-        , position = model.position - model.speed}, Cmd.none)
-      else if model.position < 0 then
-        ({ model | direction = 1
-        , position = model.position + model.speed}, Cmd.none)
-      else
-        ({ model | position = model.position + model.speed * model.direction }, Cmd.none)
-    Point ->
-      ({ model 
-      | points = model.points + 1
-      , speed = model.speed + 5
-      }, Cmd.none)
+    case msg of
+        Tick newTime ->
+            if model.position > model.viewWidth then
+                ( { model
+                    | direction = -1
+                    , position = model.position - model.speed
+                  }
+                , Cmd.none
+                )
+            else if model.position < 0 then
+                ( { model
+                    | direction = 1
+                    , position = model.position + model.speed
+                  }
+                , Cmd.none
+                )
+            else
+                ( { model | position = model.position + model.speed * model.direction }, Cmd.none )
+
+        Point ->
+            ( { model
+                | points = model.points + 1
+                , speed = model.speed + 5
+              }
+            , Cmd.none
+            )
 
 
 
@@ -69,36 +84,38 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every (100 * millisecond) Tick
+    Time.every (100 * millisecond) Tick
 
 
 
 -- VIEW
 
 
-
 view : Model -> Html Msg
 view model =
-  let
-    boxStyles : List Style
-    boxStyles =
-      [ backgroundColor "teal"
-      , borderColor "grey"
-      , borderStyle "solid"
-      , borderWidth (px 1)
-      , display block
-      , Style.width (px 50)
-      , Style.height (px 50)
-      , fontSize (pc 200)
-      , color "white"
-      , textAlign "center"
-      , position "absolute"
-      , top (px 100)
-      , left (px model.position)
-      ]
-  in
-      div []
-        [ div [class "box"
-        , style boxStyles
-        , onClick Point] [ text (toString model.points)]
-        ]
+    let
+        boxStyles : List Style
+        boxStyles =
+            [ backgroundColor "teal"
+            , borderColor "grey"
+            , borderStyle "solid"
+            , borderWidth (px 1)
+            , display block
+            , Style.width (px 50)
+            , Style.height (px 50)
+            , fontSize (pc 200)
+            , color "white"
+            , textAlign "center"
+            , position "absolute"
+            , top (px 100)
+            , left (px model.position)
+            ]
+    in
+        div []
+            [ div
+                [ class "box"
+                , style boxStyles
+                , onClick Point
+                ]
+                [ text (toString model.points) ]
+            ]
